d3ff6ac6029bb8acc9cd3a0b46d1048c330b663c
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-29 16:46:00 +0200

    feat: milestone 4 done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <script>
+import axios from "axios";
+
 export default (await import("vue")).defineComponent({
   name: "App",
 
   data() {
-    return {};
+    return {
+      projects: [],
+    };
+  },
+
+  created() {
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        console.log(response.data.results);
+        this.projects = response.data.results;
+      });
+    },
   },
 });
 </script>
 
 <template>
-  <div class="container">
-    <h1>Hello</h1>
+  <div class="container py-3">
+    <h1>All the projects</h1>
+    <hr />
+    <table class="table table-striped">
+      <thead>
+        <th>Name</th>
+        <th>Slug</th>
+        <th>Description</th>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects">
+          <td>{{ project.name }}</td>
+          <td>{{ project.slug }}</td>
+          <td>{{ project.description }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
