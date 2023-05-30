371a26808de2068d9a4e5d6edb3a99676f7d4398
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-30 09:39:16 +0200

    feat: pagination done

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ export default (await import("vue")).defineComponent({
   data() {
     return {
       projects: [],
+      currentPage: 1,
+      totalPages: 0,
     };
   },
   created() {
@@ -17,10 +19,17 @@ export default (await import("vue")).defineComponent({
   },
   methods: {
     getProjects() {
-      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-        console.log(response.data.results);
-        this.projects = response.data.results;
-      });
+      axios
+        .get("http://127.0.0.1:8000/api/projects?page=" + this.currentPage)
+        .then((response) => {
+          console.log(response.data.results);
+          this.projects = response.data.results.data;
+          this.totalPages = response.data.results.last_page;
+        });
+    },
+    fetchPage(page) {
+      this.currentPage = page;
+      this.getProjects();
     },
   },
 });
@@ -34,6 +43,26 @@ export default (await import("vue")).defineComponent({
       <div class="__card-ctn" v-for="project in projects">
         <ProjectCard :project="project"></ProjectCard>
       </div>
+    </div>
+    <div class="__page-links d-flex gap-3">
+      <a v-if="currentPage > 1" @click.prevent="fetchPage(1)">First</a>
+      <a v-if="currentPage > 1" @click.prevent="fetchPage(currentPage - 1)"
+        >Previous</a
+      >
+      <a
+        v-for="page in totalPages"
+        :key="page"
+        @click.prevent="fetchPage(page)"
+        >{{ page }}</a
+      >
+      <a
+        v-if="currentPage < totalPages"
+        @click.prevent="fetchPage(currentPage + 1)"
+        >Next</a
+      >
+      <a v-if="currentPage < totalPages" @click.prevent="fetchPage(totalPages)"
+        >Last</a
+      >
     </div>
   </div>
 </template>
@@ -51,5 +80,9 @@ export default (await import("vue")).defineComponent({
 .__card-ctn {
   width: calc(100% / 4 * 1 - (20px / 4 * 3));
   min-width: 200px;
+}
+
+a {
+  cursor: pointer;
 }
 </style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -15,24 +15,52 @@ export default {
       return this.project.description.substring(0, 100) + "...";
     },
     coverImage() {
-      return this.baseUrl + "storage/" + this.project.cover_image;
+      if (this.project.cover_image == null) {
+        return "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg";
+      } else {
+        return this.baseUrl + "storage/" + this.project.cover_image;
+      }
     },
   },
 };
 </script>
 
 <template>
-  <div class="card" style="">
+  <div class="card __project">
     <img :src="coverImage" class="card-img-top" alt="cannot retrieve image" />
-    <div class="card-body">
+    <div class="card-body d-flex flex-column gap-3">
       <h5 class="card-title">{{ project.name }}</h5>
-      {{ coverImage }}
-      <p class="card-text">
-        {{ shortDescription }}
-      </p>
+      <small
+        >Type:{{
+          project.type ? project.type.name : "Type not specified"
+        }}</small
+      >
+      <div class="__technologies d-flex gap-1">
+        <span
+          class="badge rounded-pill __badge"
+          v-for="tech in project.technologies"
+          >{{ tech.name }}</span
+        >
+      </div>
+
+      <p class="card-text">{{ shortDescription }}</p>
       <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.__project {
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+  .__technologies {
+    overflow-x: auto;
+    .__badge {
+      background-color: rgb(20, 20, 216);
+    }
+  }
+}
+</style>
